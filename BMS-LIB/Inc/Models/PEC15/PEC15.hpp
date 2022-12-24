a72a12563a710d1e3dcfc8f598395a664a5e9131
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2022-12-24 19:36:11 +0100

    separate PEC15 into cpp and hpp

--- BMS-LIB/Inc/Models/PEC15/PEC15.hpp
+++ BMS-LIB/Inc/Models/PEC15/PEC15.hpp
@@ -7,6 +7,8 @@
 
 #pragma once
 
+#include "ST-LIB.hpp"
+
 namespace PEC15 {
 	const uint8_t LENGTH = 2;
 
@@ -35,16 +37,5 @@ namespace PEC15 {
 		0x585a, 0x8ba7, 0x4e3e, 0x450c, 0x8095
 	};
 
-	uint16_t calculate(uint8_t *data, uint8_t len) {
-		uint16_t remainder, addr;
-		remainder = 16;
-
-		for (uint8_t i = 0; i<len; i++) // loops for each byte in data array
-		{
-			addr = ((remainder>>7)^data[i])&0xff; // calculate PEC table address
-			remainder = (remainder<<8)^crc15Table[addr];
-		}
-
-		return(remainder*2); // The CRC15 has a 0 in the LSB so the remainder must be multiplied by 2
-	}
+	uint16_t calculate(uint8_t *data, uint8_t len);
 }

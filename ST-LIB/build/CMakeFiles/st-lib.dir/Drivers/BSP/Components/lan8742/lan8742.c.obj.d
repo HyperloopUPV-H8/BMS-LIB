6e2e12c3562be846151e233545c15c97ece9a910
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2023-04-19 14:12:52 +0200

    make cmake and vscode env

---
+++ ST-LIB/build/CMakeFiles/st-lib.dir/Drivers/BSP/Components/lan8742/lan8742.c.obj.d
@@ -0,0 +1,10 @@
+/home/alejandro/projects/BMS/ST-LIB/build/CMakeFiles/st-lib.dir/Drivers/BSP/Components/lan8742/lan8742.c.obj: \
+ /home/alejandro/projects/ST-LIB/Drivers/BSP/Components/lan8742/lan8742.c \
+ /home/alejandro/projects/ST-LIB/Drivers/BSP/Components/lan8742/lan8742.h \
+ /usr/share/arm-gnu-toolchain-12.2.rel1-x86_64-arm-none-eabi/lib/gcc/arm-none-eabi/12.2.1/include/stdint.h \
+ /usr/share/arm-gnu-toolchain-12.2.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/stdint.h \
+ /usr/share/arm-gnu-toolchain-12.2.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/machine/_default_types.h \
+ /usr/share/arm-gnu-toolchain-12.2.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/features.h \
+ /usr/share/arm-gnu-toolchain-12.2.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/_newlib_version.h \
+ /usr/share/arm-gnu-toolchain-12.2.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/_intsup.h \
+ /usr/share/arm-gnu-toolchain-12.2.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/_stdint.h

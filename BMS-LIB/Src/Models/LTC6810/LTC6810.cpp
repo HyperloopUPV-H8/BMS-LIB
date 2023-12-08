f32dd1b15ef3b92211cef74486c1454f808dd3da
Juanito <juanchorosas27@gmail.com>
2023-12-08 23:34:27 +0100

    LTC6810 constructor fix

--- BMS-LIB/Src/Models/LTC6810/LTC6810.cpp
+++ BMS-LIB/Src/Models/LTC6810/LTC6810.cpp
@@ -1,8 +1,12 @@
 #include "LTC6810/LTC6810.hpp"
 
-LTC6810::LTC6810() : cell_voltages{ 0 }, temperatures{ 0 } {
+LTC6810::LTC6810(){
+    cell_voltages[0] = {0, 0, 0};
+    cell_voltages[1] = {0, 0, 0};
+    temperatures = {0, 0, 0}; 
     peripheral_configuration = configuration();
-}
+} 
+
 
 void LTC6810::initialize() {
     battery = Battery(&cell_voltages[0], &cell_voltages[1], &temperatures.voltage1, &temperatures.voltage2);
